4ea1449c7dc704b1d01b20cfa25a7a5b1182ba14
Tyler Nichols <aztyler77@gmail.com>
2026-06-22 18:09:39 -0600

    Implement King moves and pass all relevant tests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -176,6 +176,38 @@ public class ChessPiece {
             }
         }
 
+        if (type == PieceType.KING) {
+            int[][] kingMoves = {
+                    {1,-1},
+                    {1,0},
+                    {1,1},
+                    {0,-1},
+                    {0,1},
+                    {-1,-1},
+                    {-1,0},
+                    {-1,1}
+            };
+            for (int[] move : kingMoves) {
+                int myRow = myPosition.getRow();
+                int myCol = myPosition.getColumn();
+                int rowMove = move[0];
+                int colMove = move[1];
+                int newRow = myRow + rowMove;
+                int newCol = myCol + colMove;
+                if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
+                    ChessPosition newPosition = new ChessPosition(newRow, newCol);
+                    ChessPiece newPiece = board.getPiece(newPosition);
+                    if (newPiece == null) {
+                        pieceMoves.add(new ChessMove(myPosition, newPosition, null));
+                    } else {
+                        if (pieceColor != newPiece.getTeamColor()) {
+                            pieceMoves.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                    }
+                }
+            }
+        }
+
         return pieceMoves;
     }
 }
