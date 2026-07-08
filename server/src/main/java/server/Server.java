c2f58859359ff03905f0ca346f49fcc170a05d34
Tyler Nichols <aztyler77@gmail.com>
2026-07-08 17:40:29 -0600

    Add basic structure to Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,4 +1,21 @@
 package server;
+import io.javalin.Javalin;
 
 public class Server {
+    private final Javalin javalin;
+
+    public Server() {
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        // endpoints will  be registered here later
+    }
+
+    public int run(int targetPort) {
+        javalin.start(targetPort);
+        return javalin.port();
+    }
+
+    public void stop() {
+        javalin.stop();
+    }
 }
