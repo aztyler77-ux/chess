5ec4685b3b8eadbfd54a947944d815f1c0739e44
Tyler Nichols <aztyler77@gmail.com>
2026-07-07 20:45:17 -0600

    Implement register service within UserService.java

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,40 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
 public class UserService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request)
+            throws DataAccessException, BadRequestException, AlreadyTakenException {
+
+        if (request == null) {throw new BadRequestException("Error: bad request");}
+        if (isBlank(request.username())) {throw new BadRequestException("Error: bad request");}
+        if (isBlank(request.password())) {throw new BadRequestException("Error: bad request");}
+        if (isBlank(request.email())) {throw new BadRequestException("Error: bad request");}
+
+        UserData existingUser = userDAO.getUser(request.username());
+        if (existingUser != null) {throw new AlreadyTakenException("Error: already taken");}
+
+        UserData newUser = new UserData(request.username(), request.password(), request.email());
+        userDAO.createUser(newUser);
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, request.username());
+        authDAO.createAuth(authData);
+        return new RegisterResult(request.username(), authToken);
+    }
+
+    // helper method to detect if string value is blank
+    private boolean isBlank(String value) {return value == null || value.isBlank();}
 }
