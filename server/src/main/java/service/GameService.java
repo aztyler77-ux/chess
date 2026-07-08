9604b0a5dd8b334b30471524757c2f824de4bd6e
Tyler Nichols <aztyler77@gmail.com>
2026-07-07 22:20:04 -0600

    Implement logic to create game service

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,40 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.AuthData;
+
 public class GameService {
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request)
+            throws DataAccessException, BadRequestException, UnauthorizedException {
+
+        if (request == null || isBlank(request.gameName())) {
+            throw new BadRequestException("Error: bad request");
+        }
+
+        if (isBlank(request.authToken())) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        AuthData authData = authDAO.getAuth(request.authToken());
+        if (authData == null) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        int gameID = gameDAO.createGame(request.gameName());
+        return new CreateGameResult(gameID);
+    }
+
+    private boolean isBlank(String value) {
+        return value == null || value.isBlank();
+    }
 }
