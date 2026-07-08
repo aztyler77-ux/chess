c4673e41be8ebc55219e2b00b008dca9f424139d
Tyler Nichols <aztyler77@gmail.com>
2026-07-08 16:22:08 -0600

    Create JoinGameRequest.java and implement it into GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -4,6 +4,7 @@ import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import model.AuthData;
+import model.GameData;
 
 public class GameService {
     private final GameDAO gameDAO;
@@ -46,6 +47,50 @@ public class GameService {
             throw new UnauthorizedException("Error: unauthorized");
         }
         return new ListGamesResult(gameDAO.listGames());
+    }
+
+    public void joinGame(JoinGameRequest request) throws DataAccessException, BadRequestException, UnauthorizedException, AlreadyTakenException {
+        if (request == null) {throw new BadRequestException("Error: bad request");}
+        if (isBlank(request.authToken())) {throw new UnauthorizedException("Error: unauthorized");}
+
+        AuthData authData = authDAO.getAuth(request.authToken());
+        if (authData == null) {throw new UnauthorizedException("Error: unauthorized");}
+        if (request.gameID() == null) {throw new BadRequestException("Error: bad request");}
+        if (!"WHITE".equals(request.playerColor()))
+            if (!"BLACK".equals(request.playerColor())) {
+                throw new BadRequestException("Error: bad request");}
+
+        GameData gameData = gameDAO.getGame(request.gameID());
+        if (gameData == null) {throw new BadRequestException("Error: bad request");}
+        if ("WHITE".equals(request.playerColor())) {
+            if (gameData.whiteUsername() != null) {
+                throw new AlreadyTakenException("Error: already taken");
+            }
+
+            GameData updatedGame = new GameData(
+                    gameData.gameID(),
+                    authData.username(),
+                    gameData.blackUsername(),
+                    gameData.gameName(),
+                    gameData.game()
+            );
+            gameDAO.updateGame(updatedGame);
+        }
+
+        if ("BLACK".equals(request.playerColor())) {
+            if (gameData.blackUsername() != null) {
+                throw new AlreadyTakenException("Error: already taken");
+            }
+
+            GameData updatedGame = new GameData(
+                    gameData.gameID(),
+                    gameData.whiteUsername(),
+                    authData.username(),
+                    gameData.gameName(),
+                    gameData.game()
+            );
+            gameDAO.updateGame(updatedGame);
+        }
     }
 
     private boolean isBlank(String value) {
