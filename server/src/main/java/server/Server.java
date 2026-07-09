f19e54476315dd9c296324089f4c60b9b95a8350
Tyler Nichols <aztyler77@gmail.com>
2026-07-08 21:31:12 -0600

    Add register endpoint and organize Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,25 +11,44 @@ import service.GameService;
 import service.UserService;
 import dataaccess.DataAccessException;
 import java.util.Map;
+import service.RegisterRequest;
+import service.BadRequestException;
+import dataaccess.DataAccessException;
+import com.google.gson.Gson;
+import service.AlreadyTakenException;
+
+
+
 
 public class Server {
-    private final GameService gameService;
+    // data stores for server
     private final UserDAO userDAO;
-    private final ClearService clearService;
     private final AuthDAO authDAO;
-    private final UserService userService;
     private final GameDAO gameDAO;
+    // Services for app logic
+    private final ClearService clearService;
+    private final UserService userService;
+    private final GameService gameService;
+    // Gson to convert between JSON & Java
+    private final Gson gson;
+    // Javalin for HTTP server / routing
     private final Javalin javalin;
 
     public Server() {
+        // memory DAO group
         userDAO = new MemoryUserDAO();
         authDAO = new MemoryAuthDAO();
         gameDAO = new MemoryGameDAO();
+        // services connect to DOAs
         clearService = new ClearService(userDAO, authDAO, gameDAO);
         userService = new UserService(userDAO, authDAO);
         gameService = new GameService(gameDAO, authDAO);
+        gson = new Gson();
+
+        // jav serves web files
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
+        // http endpoints
         javalin.delete("/db", ctx -> {
             try {
                 clearService.clear();
@@ -37,14 +56,24 @@ public class Server {
             } catch (DataAccessException error) {
                 ctx.status(500).json(Map.of("message", "Error: " + error.getMessage()));
             }
+
+        });
+
+        javalin.post("/user", ctx -> {
+            try {
+                RegisterRequest request = gson.fromJson(ctx.body(), RegisterRequest.class);
+                ctx.status(200).json(userService.register(request));
+            } catch (BadRequestException error) {ctx.status(400).json(Map.of("message", error.getMessage()));
+            } catch (DataAccessException error) {ctx.status(500).json(Map.of("message", "Error: " + error.getMessage()));
+            } catch (AlreadyTakenException error) {ctx.status(403).json(Map.of("message", error.getMessage()));
+            }
         });
     }
-
+    // start & stop server
     public int run(int targetPort) {
         javalin.start(targetPort);
         return javalin.port();
     }
-
     public void stop() {
         javalin.stop();
     }
